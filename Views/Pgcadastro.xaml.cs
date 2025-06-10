021b72ad2ab72edb54f042dea4d7b99bf805238a
Joao Lima <joao.lima@aluno.unifafibe.edu.br>
2025-06-10 19:16:03 -0300

    Feito Cadastro de veiculos e alterações na tela principal

--- Views/Pgcadastro.xaml.cs
+++ Views/Pgcadastro.xaml.cs
@@ -1,19 +1,118 @@
 namespace parkSmart.Views;
+using parkSmart.Controllers;
+using parkSmart.Models;
+using parkSmart.Services;
 
 public partial class Pgcadastro : ContentPage
 {
-	public Pgcadastro()
+    private VeiculosController veiculosController ;
+
+    public Pgcadastro()
 	{
 		InitializeComponent();
-	}
 
-    private void btnSelecionarFoto_Clicked(object sender, EventArgs e)
+        veiculosController = new VeiculosController();
+    }
+
+
+    private string sImagemSelecionada;
+    private async void btnSelecionarFoto_Clicked(object sender, EventArgs e)
     {
-
+        sImagemSelecionada =
+           await ImageService.SelecionarImagem();
+        //Apresentar a imagem ao usuario
+        FotoImage.Source = sImagemSelecionada;
+        //Exibir o botão remover
+        btnRemover.IsVisible = true;
     }
 
     private void btnSalvar_Clicked(object sender, EventArgs e)
     {
+        string placa = txtPlaca.Text;
+        string marca = txtMarca.Text;
+        string modelo = txtModelo.Text;
+        string cor = txtCor.Text;
+        string nomeProprietario = txtNomeProprietario.Text;
+        string tipoPlano = TipoPicker.SelectedItem?.ToString();
 
+        DateTime dataEntrada = datePickerEntrada.Date + timePickerEntrada.Time;
+        DateTime dataSaida = datePickerSaida.Date + timePickerSaida.Time;
+
+        //Validar os registro
+        if (string.IsNullOrEmpty(placa) ||
+            string.IsNullOrEmpty(marca)) 
+                
+        {
+            //Se um dos dois estiver vazio
+            //ja abortamos a rotina
+            DisplayAlert(
+                "Atençao",
+                "Preencha os campos corretamente.",
+                "OK");
+            //Abortar a rotina
+            return;
+        }
+
+        //Iremos criar e popular o objeto pessoa
+        Veiculos veiculo = new Veiculos();
+        veiculo.Placa = placa;
+        veiculo.Marca = marca;
+        veiculo.Modelo = modelo;
+        veiculo.Cor = cor;
+        veiculo.NomeProprietario = nomeProprietario;
+        veiculo.TipoPlano = tipoPlano;
+        veiculo.FotoVeic = ImageService.CopiarImagem(sImagemSelecionada);
+        veiculo.DataEntrada = dataEntrada;
+        veiculo.DataSaida = dataSaida;
+        veiculo.Pago = checkPago.IsChecked;
+
+        //Chamara a rotina para copiar a imagem
+        //e iremos gravar no banco
+        //o diretorio da nova imagem(copia)
+        veiculo.FotoVeic =
+            ImageService.CopiarImagem(sImagemSelecionada);
+
+        //Realizar a inserção no banco de dados
+        if (veiculosController.Insert(veiculo))
+        {
+            //Notificamos que deu tudo certo
+            DisplayAlert(
+                "Informação",
+                "Registro salvo com sucesso.",
+                "OK");
+            txtPlaca.Text = "";
+            txtMarca.Text = "";
+            txtModelo.Text = "";
+            txtCor.Text = "";
+            txtNomeProprietario.Text = "";
+            checkPago.IsChecked = false;
+            TipoPicker.SelectedIndex = -1;
+            datePickerEntrada.Date = DateTime.Now;
+            timePickerEntrada.Time = TimeSpan.Zero;
+            datePickerSaida.Date = DateTime.Now;
+            timePickerSaida.Time = TimeSpan.Zero;
+            LimparImagem();
+        }
+        else
+        {
+            DisplayAlert(
+                "Erro",
+                "Falha ao salvar o registro no " +
+                "banco de dados",
+                "OK");
+        }
+    }
+
+    private void LimparImagem()
+    {
+        //Remover a imagem da tela
+        FotoImage.Source = "";
+        //Ocultar o botão Remover
+        btnRemover.IsVisible = false;
+    }
+
+    private void btnRemover_Clicked(object sender, EventArgs e)
+    {
+        LimparImagem();
     }
 }
